d246bc54049484d34bf46e3cf1487cacd394ff12
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-02 15:36:40 -0600

    Add best_friend2 and modify if statement

--- friends.rb
+++ friends.rb
@@ -1,8 +1,11 @@
 best_friend = "Arthur"
-
+best_friend2 = "Aidan"
 
 if best_friend == "Joseph"
-    puts "WHAT'S UP BRO?!"
+    puts "WHAT'S UP JOSEPH?!"
+elsif best_friend2 == "Aidan"
+    puts "HOW'S IT GOING MY BROTHER AIDAN!"
 else
     puts "WHO THE HELL IS #{best_friend}?!"
 end 
+
